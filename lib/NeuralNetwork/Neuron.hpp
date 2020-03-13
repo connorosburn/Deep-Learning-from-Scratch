aed8003243c3db2b1ba3a0c22d707069c70bec71
connorosburn <cogeneral92@gmail.com>
2020-03-12 22:32:57 -0400

    Lays out a basic neuron interface

---
+++ lib/NeuralNetwork/Neuron.hpp
@@ -0,0 +1,22 @@
+#ifndef NEURON_HPP
+#define NEURON_HPP
+
+class Neuron {
+    public:
+        Neuron(std::vector<const double&> backOutputs, std::vector<const double&> backErrors);
+        double productSum();
+        void forwardPropogate(std::function<double(double)> activation);
+        void passToWeights(double delta, const double& learningRate);
+        void backPropogate(std::function<double(double)> activationDerivative, const double& learningRate);
+        const double& getOutput();
+        double& getError();
+
+    private:
+        std::vector<Weight> weights;
+        double bias;
+        double output;
+        double error;
+
+};
+
+#endif
