595941eba6cc4c1e4ab265e1a7d5244442c3c840
connorosburn <cogeneral92@gmail.com>
2020-03-14 14:13:21 -0400

    Lays out a silly binary solver to test/find awful mistakes in code thus far

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include "Layer/FullyConnectedLayer.hpp"
+
+std::vector<double> randomBinary() {
+
+}
+
+double binarySum(std::vector<double> binaryDigits) {
+    
+}
+
+struct BinaryData {
+    BinaryData() {
+        binaryDigits = randomBinary(10);
+        baseTenSum = binarySum(binaryDigits);
+    }
+    std::vector<double> binaryDigits;
+    double baseTenSum;
+};
 
 int main() {
     // machine learns here
