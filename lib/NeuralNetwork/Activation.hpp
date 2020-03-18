a80b4346faf26c4e09313b6d2f75e8c471afee48
connorosburn <cogeneral92@gmail.com>
2020-03-17 20:20:13 -0400

    keeps the binary addition base 2 by using sigmoid output nodes and binary cross entropy to determine and train based on each individual digit

--- lib/NeuralNetwork/Activation.hpp
+++ lib/NeuralNetwork/Activation.hpp
@@ -1,6 +1,17 @@
 #ifndef ACTIVATION_HPP
 #define ACTIVATION_HPP
 
+#include <cmath>
+/*
+    NOTE: Activation derivatives currently take the 
+    output that has already been run through the original 
+    activation function, since all derivatives of activation
+    functions CURRENTLY in use can be calculated from that value. 
+    This avoids certain redundancies, but if nessecary, I'll go back
+    and change it.
+*/
+
+
 namespace Activation {
     struct Activation {
         Activation(std::function<double(double)> act, std::function<double(double)> der):
@@ -29,6 +40,19 @@ namespace Activation {
             } else {
                 return 0;
             }
+        }
+    );
+
+    const Activation sigmoid (
+
+        //sigmoid
+        [](double x) -> double {
+            return double(1) / (double(1) + std::exp(double(-1) * x));
+        },
+        
+        //sigmoid derivative
+        [](double x) -> double {
+            return x * (double(1) - x);
         }
     );
 }
--- lib/main.cpp
+++ lib/main.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <random>
 #include "NeuralNetwork/Layer/FullyConnectedLayer.hpp"
+#include "NeuralNetwork/Activation.hpp"
 #include <functional>
 #include <vector>
 #include <typeinfo>
@@ -32,41 +33,61 @@ double baseTenFromBinary(std::vector<double> binaryDigits) {
     return sum;
 }
 
-double binarySum(std::vector<double> binaryDigits) {
+std::vector<double> binarySum(std::vector<double> binaryDigits) {
     auto halfIter = binaryDigits.begin() + (binaryDigits.size() / 2);
     std::vector<double> firstBinary = std::vector<double>(binaryDigits.begin(), halfIter);
     std::vector<double> secondBinary = std::vector<double>(halfIter, binaryDigits.end());
-    return baseTenFromBinary(firstBinary) + baseTenFromBinary(secondBinary);
+    std::vector<double> sum(5);
+    int carryOver = 0;
+    for(int i = firstBinary.size() - 1; i >= 0; i--) {
+        int digitSum = firstBinary[i] + secondBinary[i] + carryOver;
+        sum[4 - (firstBinary.size() - i - 1)] = digitSum % 2;
+        carryOver = digitSum / 2;
+    }
+    sum[0] = carryOver;
+    return sum;
 }
 
 struct BinaryData {
     BinaryData() {
         binaryDigits = randomBinary(8);
-        baseTenSum = binarySum(binaryDigits);
+        sum = binarySum(binaryDigits);
     }
     std::vector<double> binaryDigits;
-    double baseTenSum;
+    std::vector<double> sum;
 };
 
 void binaryCrossEntropyDerivative(const std::vector<double>& prediction, const std::vector<double>& expectation, std::vector<std::reference_wrapper<double>> errorRef) {
     for(int i = 0; i < prediction.size(); i++) {
-        if(expectation[i] == 1) {
-            errorRef[i] = double(1) / prediction[i];
+        if(expectation[i] > 0) {
+            errorRef[i].get() = double(-1) * (double(1) / prediction[i]);
         } else {
-            errorRef[i] = double(1) / (double(1) - prediction[i]);
+            errorRef[i].get() = double(1) / (double(1) - prediction[i]);
         }
     }
 }
 
+std::vector<double> interpretNetworkOutput(const std::vector<double>& networkOutput) {
+    std::vector<double> output;
+    for(const double& probability : networkOutput) {
+        if(probability >= 0.5) {
+            output.push_back(1);
+        } else {
+            output.push_back(0);
+        }
+    }
+    return output;
+}
+
 int main() {
-    const double LEARNING_RATE = 0.000001;
+    const double LEARNING_RATE = 0.001;
 
     std::vector<double> input(8, 0);
     std::vector<double> totalError(8, 0);
-    FullyConnectedLayer hiddenLayer({input.begin(), input.end()}, {totalError.begin(), totalError.end()}, 8, Activation::relu);
+    FullyConnectedLayer hiddenLayer({input.begin(), input.end()}, {totalError.begin(), totalError.end()}, 16, Activation::relu);
     FullyConnectedLayer outputLayer(hiddenLayer.getOutputs(), hiddenLayer.getErrors(), 5, Activation::sigmoid);
-    auto& networkOutput = outputLayer.getOutputs();
-    auto& networkError = outputLayer.getErrors();
+    auto networkOutput = outputLayer.getOutputs();
+    auto networkError = outputLayer.getErrors();
 
     while(true) {
         // generates binary data and forward propogates it
@@ -76,28 +97,28 @@ int main() {
         outputLayer.forwardPropogate();
         
         // reports prediction
-        std::vector<double> prediction = interpretNetworkOutput(networkOutput);
+        std::vector<double> prediction = interpretNetworkOutput(std::vector<double>(networkOutput.begin(), networkOutput.end()));
         std::cout<<"\n";
         std::cout<<"Input: ";
         for(const double& digit : binaryData.binaryDigits) {
             std::cout<<digit;
         }
         std::cout<<"\nExpectation: ";
-        for(const double& digit : binaryData.binarySum) {
+        for(const double& digit : binaryData.sum) {
             std::cout<<digit;
         }
         std::cout<<"\nPrediction: ";
         for(const double& digit : prediction) {
             std::cout<<digit;
         }
-        std::cout<<"\nRaw Outputs:"
+        std::cout<<"\nRaw Outputs:";
         for(int i = 0; i < networkOutput.size(); i++) {
-            std::cout<<"\n"<<networkOutput[i]<<"("<<prediction[i]<<")";
+            std::cout<<"\n"<<networkOutput[i]<<"("<<binaryData.sum[i]<<")";
         }
-        std::cout<<"\n"
+        std::cout<<"\n";
 
         // trains model
-        binaryCrossEntropyDerivative(networkOutput, binaryData.binarySum, networkError);
+        binaryCrossEntropyDerivative(std::vector<double>(networkOutput.begin(), networkOutput.end()), binaryData.sum, networkError);
         outputLayer.backPropogate(LEARNING_RATE);
         hiddenLayer.backPropogate(LEARNING_RATE);
     }
