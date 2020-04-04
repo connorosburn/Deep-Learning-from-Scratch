3aaebf7a80d716e5cfd09652ce2e9d98613e3dd9
connorosburn <cogeneral92@gmail.com>
2020-04-04 18:33:59 -0400

    cmake support

---
+++ CMakeFiles/mnist.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/MNISTFashion/MNISTLoader.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/MNISTFashion/MNISTLoader.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/NeuralNetwork/Layer/ConvolutionalLayer.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/ConvolutionalLayer.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/NeuralNetwork/Layer/Layer.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/Layer.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/NeuralNetwork/Layer/MaxPoolingLayer.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/MaxPoolingLayer.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/NeuralNetwork/Layer/SoftmaxLayer.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/SoftmaxLayer.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/NeuralNetwork/Neuron.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/NeuralNetwork/Neuron.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/NeuralNetwork/Weight.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/NeuralNetwork/Weight.cpp.o"
+  "/Users/connorosburn/some_promises/mnist_classifier/lib/main.cpp" "/Users/connorosburn/some_promises/mnist_classifier/CMakeFiles/mnist.dir/lib/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/mnist.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mnist.dir/lib/MNISTFashion/MNISTLoader.cpp.o"
+  "CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/ConvolutionalLayer.cpp.o"
+  "CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/Layer.cpp.o"
+  "CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/MaxPoolingLayer.cpp.o"
+  "CMakeFiles/mnist.dir/lib/NeuralNetwork/Layer/SoftmaxLayer.cpp.o"
+  "CMakeFiles/mnist.dir/lib/NeuralNetwork/Neuron.cpp.o"
+  "CMakeFiles/mnist.dir/lib/NeuralNetwork/Weight.cpp.o"
+  "CMakeFiles/mnist.dir/lib/main.cpp.o"
+  "mnist"
+  "mnist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mnist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
